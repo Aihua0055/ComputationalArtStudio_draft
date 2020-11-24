3707f729f97c48b10b4942ef9aed67429a3ccfc1
Aihua Chen <aihua@pdx.edu>
2020-11-23 21:12:38 -0800

    Added:use Jinja 2 add jumbotron for each page

--- app.py
+++ app.py
@@ -15,7 +15,37 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 def main():
     kwargs={
         'title':'Home',
+        'jumbotron':{
+            "header":"Image Style Playground",
+            "bg_image":"static/images/bg1450496.jpg",
+        "text":"Add text"
+        },
+
+        'homePageContents':[{
+        "Title":"Style Transfer",
+        "Text": "Neural Style Transfer (NST) refers to a class of software algorithms that manipulate digital images, or videos, in order to adopt the appearance or visual style of another image. NST algorithms are characterized by their use of deep neural networks for the sake of image transformation. Common uses for NST are the creation of artificial artwork from photographs, for example by transferring the appearance of famous paintings to user-supplied photographs. Several notable mobile apps use NST techniques for this purpose, including DeepArt and Prisma. This method has been used by artists and designers around the globe to develop new artwork based on existent style(s). --Wikipedia Neural Style Transfer",
+        "imagePath":"static/images/styleTransfer.jpeg",
+        "Link":"/styletransfer",
+        "Action":"+ Go Style Transfer"
+    },
+    {
+
+        "Title": "Color Harmonization",
+        "Text": "In color theory, color harmony refers to the property that certain aesthetically pleasing color combinations have. These combinations create pleasing contrasts and consonances that are said to be harmonious. These combinations can be of complementary colors, split-complementary colors, color triads, or analogous colors. Color harmony has been a topic of extensive study throughout history, but only since the Renaissance and the Scientific Revolution has it seen extensive codification. Artists and designers make use of these harmonies in order to achieve certain moods or aesthetics.--Wikipedia Neural Style Transfer",
+        "imagePath":"static/images/colorHar.png",
+        "Link":"/colorharm",
+        "Action":"+ Go Color Harmonization"
+    },{
+        "Title":"Gallery",
+        "Text":"In our Gallery, you can look at style transfer and color harmonization products.",
+        "imagePath":"static/images/gallery.png",
+        "Link": "/gallery",
+        "Action":"+ Go to gallery"
     }
+    ]
+
+        }
+
     return render_template('home.html',**kwargs)
     
 
@@ -23,20 +53,54 @@ def main():
 def about():
     kwargs={
         'title':'About',
+        'jumbotron':{
+            "header":"AI & Art",
+            "bg_image":"static/images/bg1450496.jpg",
+            "text": "Add text"
+        }
     }
     return render_template('about.html',**kwargs)
 
 @app.route("/colorharm")
 def colorharm():
-    return render_template('colorharm.html')
+    kwargs={
+        'title':'Color Harmonization',
+        'jumbotron':{
+            "header":"Color Harmonization",
+            "bg_image":"static/images/bg1450496.jpg",
+            "text": "Add text"
+
+        }
+    }
+    return render_template('colorharm.html',**kwargs)
 @app.route("/styletransfer")
 def styletransfer():
-    return render_template('styletransfer.html')
+    kwargs={
+        'title':'Style Transfer',
+        'jumbotron':{
+            "header":"Style Transfer",
+            "bg_image":"static/images/bg1450496.jpg",
+            "text": "Add text"
+
+        }
+    }
+    return render_template('styletransfer.html',**kwargs)
 
 @app.route("/gallery")
 def gallery():
     response = requests.get('https://restcountries.eu/rest/v2/all')
-    return render_template('gallery.html',recipes= response.json())
+    kwargs={
+        'title':'About',
+        'jumbotron':{
+            "header":"Computational Art Gallery",
+            "bg_image":"static/images/bg1450496.jpg",
+            "text": "Add text"
+
+        },
+        'recipes' : response.json()
+    }
+    return render_template('gallery.html',**kwargs)
+
 
 
 # upload selected image and forward to processing page
